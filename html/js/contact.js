dd4f8a82e0f191a6c690cb1fd727d65c81994e82
LevequeLoic <loic.leveque@etu-u.bordeaux.fr>
2022-04-07 00:31:42 +0200

    partieFonctionnelle OK anim a voir

--- html/js/contact.js
+++ html/js/contact.js
@@ -1,11 +1,36 @@
-function affichageValidity() {
-    var champs = document.querySelectorAll(".champ");
+function affichageValidity(appel) {
+    var champs = document.querySelectorAll(".pChamps");
+    console.log('dud')
     Array.from(champs).forEach(c => {
-        if(c.checkValidity() && c.value != ''){
-            c.style.backgroundColor = 'green';
+        let input = c.getElementsByClassName("champ");
+        if(input[0].checkValidity() && input[0].value != ''){
+            if(getComputedStyle(c.getElementsByClassName("validImg")[0]).zIndex==-1){
+               animationLogosForm(c.getElementsByClassName("validImg")[0]);
+               c.getElementsByClassName("invalidImg")[0].style.zIndex = -1;  
+            }
+               
         }
-        else {
-            c.style.backgroundColor = 'red';
+        else if(input[0].value != '' || appel == "buttonCall") {
+            if(getComputedStyle(c.getElementsByClassName("invalidImg")[0]).zIndex==-1){
+                animationLogosForm(c.getElementsByClassName("invalidImg")[0]);
+                c.getElementsByClassName("validImg")[0].style.zIndex = -1;
+            }
+
+            
         }
     });
+}
+
+
+function animationLogosForm(elt) {
+    elt.animate([{
+        zIndex : 1,
+        opacity : 0
+    },
+    {
+        zIndex : 1,
+        opacity : 1
+    }],{duration:2000});
+    elt.style.zIndex = 1;
+    elt.style.opacity = 1;
 }
