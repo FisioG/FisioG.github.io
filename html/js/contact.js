3984fe9cc9925a100e08bd263d7d44f5d1befefe
LevequeLoic <loic.leveque@etu-u.bordeaux.fr>
2022-04-07 00:52:34 +0200

    animation OK

--- html/js/contact.js
+++ html/js/contact.js
@@ -5,15 +5,15 @@ function affichageValidity(appel) {
         let input = c.getElementsByClassName("champ");
         if(input[0].checkValidity() && input[0].value != ''){
             if(getComputedStyle(c.getElementsByClassName("validImg")[0]).zIndex==-1){
-               animationLogosForm(c.getElementsByClassName("validImg")[0]);
-               c.getElementsByClassName("invalidImg")[0].style.zIndex = -1;  
+               animationLogosFormApparait(c.getElementsByClassName("validImg")[0]);
+               animationLogosFormDisparait(c.getElementsByClassName("invalidImg")[0]);  
             }
                
         }
         else if(input[0].value != '' || appel == "buttonCall") {
             if(getComputedStyle(c.getElementsByClassName("invalidImg")[0]).zIndex==-1){
-                animationLogosForm(c.getElementsByClassName("invalidImg")[0]);
-                c.getElementsByClassName("validImg")[0].style.zIndex = -1;
+                animationLogosFormApparait(c.getElementsByClassName("invalidImg")[0]);
+                animationLogosFormDisparait(c.getElementsByClassName("validImg")[0]);
             }
 
             
@@ -22,15 +22,28 @@ function affichageValidity(appel) {
 }
 
 
-function animationLogosForm(elt) {
+function animationLogosFormApparait(elt) {
     elt.animate([{
         zIndex : 1,
-        opacity : 0
+        opacity : getComputedStyle(elt).opacity
     },
     {
         zIndex : 1,
         opacity : 1
-    }],{duration:2000});
+    }],{duration:1500});
     elt.style.zIndex = 1;
     elt.style.opacity = 1;
+}
+
+function animationLogosFormDisparait(elt) {
+    elt.animate([{
+        zIndex : 1,
+        opacity : getComputedStyle(elt).opacity
+    },
+    {
+        zIndex : 1,
+        opacity :0
+    }],{duration:1000});
+    elt.style.zIndex = -1;
+    elt.style.opacity = 0;
 }
