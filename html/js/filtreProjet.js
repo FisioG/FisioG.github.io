7f45b66605fceed0351ac1c1544d2002bf8baf12
LevequeLoic <loic.leveque@etu-u.bordeaux.fr>
2022-10-11 16:29:56 +0200

    Ajout de l'animation de la filtration de projet

--- html/js/filtreProjet.js
+++ html/js/filtreProjet.js
@@ -3,11 +3,12 @@ function filtrage(language) {
     if(icone != null) {icone.classList.remove('selectionne');}
     let projets = document.querySelectorAll('.projet');
     Array.from(projets).forEach(element => {
-        element.style.display = 'none';
+        element.classList.add('projet--hidden')
+        element.classList.add('projet--transition')
     });
     projets = document.querySelectorAll('.'+language);
     Array.from(projets).forEach(element => {
-        element.style.display = 'flex';
+        element.classList.remove('projet--hidden')
     });
     icone = document.querySelector('.img'+language);
     icone.classList.add('selectionne');
@@ -18,7 +19,7 @@ function verifSelect(language) {
     if(icone != null && icone.classList.contains('img'+language)) {
         let projets = document.querySelectorAll('.projet');
         Array.from(projets).forEach(element => {
-            element.style.display = 'flex';
+            element.classList.remove('projet--hidden')
         });
         icone.classList.remove('selectionne');
     }
