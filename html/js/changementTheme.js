9ea58ce64ab78391f68c351b331e8517b7cc962e
LevequeLoic <loic.leveque@etu-u.bordeaux.fr>
2022-10-13 08:41:08 +0200

    Ajout animation changement de theme

--- html/js/changementTheme.js
+++ html/js/changementTheme.js
@@ -1,42 +1,28 @@
 function changeTheme() {
     console.log(localStorage.getItem('theme'))
     if (localStorage.getItem('theme') == 'sombre') {
-        document.querySelector("#P1").style.backgroundColor = "#f4edea";
-        document.querySelector("#P2").style.backgroundColor = "#F4D1AE";
-        document.querySelector("#P2").style.color = "black";
-        document.querySelector(".footer").style.backgroundColor = '#c5d8d1';
-        document.querySelector("#logoMode").style.backgroundColor = '#0c717e';
-        document.querySelector("#menuSlide").style.backgroundColor = '#c5d8d1';
-        let z = document.querySelectorAll(".txtLien")
-        z.forEach(element => {
-            element.style.color = 'black';
-        });
-        let proj = document.querySelectorAll(".projet")
-        proj.forEach(element => {
-            element.style.background = 'linear-gradient(to right,#0c717e,#c5d8d1)';
-        });
+        let projets = document.querySelectorAll('.themes');
+        Array.from(projets).forEach(element => {
+            var id = element.id
+            element.classList.remove(id+'--darkTheme')
+            element.classList.add(id+'--lightTheme')
+        });    
         localStorage.setItem('theme', 'clair');
         animLogo();
-        document.getElementById('imgMode').src = 'images/logoLune.png';
+        document.getElementById('imgMode').src = 'images/logoSoleil.png';
+
+
     }
     else {
-        document.querySelector("#P1").style.backgroundColor = "#f4d1ae";
-        document.querySelector("#P2").style.backgroundColor = "#12263a";
-        document.querySelector(".footer").style.backgroundColor = '#c5d8d1';
-        document.querySelector("#logoMode").style.backgroundColor = 'wheat';
-        document.querySelector("#menuSlide").style.backgroundColor = '#12263a';
-        let z = document.querySelectorAll(".txtLien")
-        z.forEach(element => {
-            element.style.color = 'white';
-        });
-        let proj = document.querySelectorAll(".projet")
-        proj.forEach(element => {
-            element.style.background = 'linear-gradient(to right,#c5d8d1,#0c717e)';
-        });
+        let projets = document.querySelectorAll('.themes');
+        Array.from(projets).forEach(element => {
+            var id = element.id
+            element.classList.remove(id+'--lightTheme')
+            element.classList.add(id+'--darkTheme')
+        }); 
         localStorage.setItem('theme', 'sombre');
         animLogo();
-        document.querySelector(".footer").style.backgroundColor = '#0c717e';
-        document.getElementById('imgMode').src = 'images/logoSoleil.png';
+        document.getElementById('imgMode').src = 'images/logoLune.png';
     }
 }
 
